7db1f0b29d49400dd1a2e33c03f71a80fd6167de
ProfessorAlessandromartinsdelima2103 <130934027+ProfessorAlessandromartinsdelima2103@users.noreply.github.com>
2024-06-06 21:07:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -81,16 +82,19 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function(){
-                atual++;
-                mostraPergunta();
-            })
-            caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
