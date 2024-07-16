66cb64d80347133f5025932dbe9591d810952e6f
ProfessorAlessandromartinsdelima2103 <130934027+ProfessorAlessandromartinsdelima2103@users.noreply.github.com>
2024-07-16 13:00:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import { aleatorio, nome } from './aleatorio.js';
-import { perguntas } from './perguntas.js';
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -10,73 +7,121 @@ const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
-let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
+const perguntas = [
+    {
+      enunciado:
+        "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+      alternativas: [
+        {
+          texto: "Isso é assustador!",
+          afirmacao: "No início achou assustador pensar na velocidade na qual a tecnologia está avançando."
+        },
+        {
+          texto: "Isso é maravilhoso!",
+          afirmacao:  "No início quis saber como usar IA no seu dia a dia."
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+      alternativas: [
+        {
+          texto:
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+          afirmacao: "Depois, conseguiu utilizar a IA para buscar informações úteis."
+        },
+        {
+          texto:
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+          afirmacao: "Assim sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+      alternativas: [
+        {
+          texto:
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+          afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+        },
+        {
+          texto:
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+          afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+      alternativas: [
+        {
+          texto:
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+          afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+        },
+        {
+          texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+          afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+        },
+      ],
+    },
+    {
+      enunciado:
+        "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+      alternativas: [
+        {
+          texto:
+            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+          afirmacao: "Porém, infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+        },
+        {
+          texto:
+            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+          afirmacao: "Agora se tornou conscientizado sobre o uso da IA e passa a dominá-la com especialidade"
+        },
+      ],
+    },
+  ];
 
-botaoIniciar.addEventListener('click', iniciaJogo);
+  let atual = 0;
+  let perguntaAtual;
+  let historiaFinal = "";
 
-function iniciaJogo() {
-    atual = 0;
-    historiaFinal = "";
-    telaInicial.style.display = 'none';
-    caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classList.remove("mostrar");
-    caixaResultado.classList.remove("mostrar");
-    mostraPergunta();
-}
-
-function mostraPergunta() {
+  function mostraPergunta() {
     if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
+     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
-    if (opcaoSelecionada.proxima !== undefined) {
-        atual = opcaoSelecionada.proxima;
-    } else {
-        mostraResultado();
-        return;
-    }
+    atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = `Em 2049, ${nome}`;
+    caixaPerguntas.textContent = "Em 2049, Gabriel… Achou assustador pensar que máquinas agora poderiam mudar o mundo. Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos. Percebeu que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    caixaResultado.classList.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-function jogaNovamente() {
-    atual = 0;
-    historiaFinal = "";
-    caixaResultado.classList.remove("mostrar");
-    mostraPergunta();
-}
-
-function substituiNome() {
-    for (const pergunta of perguntas) {
-        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
-    }
-}
-
-substituiNome();
+mostraPergunta();
